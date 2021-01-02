2b9da7f2970e7052a270e569b1521ba8c09260b5
Kimi Meng <kimimeng2013@gmail.com>
2021-01-02 15:54:29 -0800

    default path to movie

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import InputRequired, Length
 class RegisterForm(Form):
   nameEN = StringField('name-en', validators=[InputRequired(), Length(max=100)])
   nameCN = StringField('name-cn', validators=[Length(max=100)])
-  path = StringField('path', validators=[Length(max=100)])
+  path = StringField('path', default='movie', validators=[Length(max=100)])
   short = StringField('shortname', validators=[InputRequired(), Length(max=20)])
   file = FileField('file', validators=[Length(max=100)])
   submit = SubmitField('submit')
