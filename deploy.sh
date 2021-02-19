6846a36e59d8d8ba8d96e8109626061751e4c931
Kimi Meng <kimimeng2013@gmail.com>
2021-02-19 14:25:27 -0800

    added deploy and requirements

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -d "venv" ]
+then
+    source venv/bin/activate
+    whereis python
+    pip install -r requirements.txt
+    mysql.server restart
+    mysql -uroot -proot &
+    python3 manage.py
+else
+    virtualenv venv
+    source venv/bin/activate
+    python3 -m pip install --upgrade pip
+    pip install -r requirements.txt
+    mysql.server restart
+    mysql -uroot -proot &
+    python3 manage.py
+fi
